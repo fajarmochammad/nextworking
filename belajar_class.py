118654608a1cd329185915117c3e94b40604c06b
fajarmochammad <fajarmochammad@gmail.com>
2021-07-07 10:28:05 +0700

    request

--- belajar_class.py
+++ belajar_class.py
@@ -1,7 +1,7 @@
 import requests
 
 try:
-    r = requests.get('https://fac ebook.com')
+    r = requests.get('https://facebook.com')
     print (r.status_code)
     if r.status_code == 200:
         print (r.text)
