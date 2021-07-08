80f9430f3fd13d9bae607841c383e9c6b62273b2
fajarmochammad <fajarmochammad@gmail.com>
2021-07-08 16:54:49 +0700

    commit2

---
+++ use_geometri.py
@@ -0,0 +1,5 @@
+from geometri.segitiga import hitung_luas_segitiga
+#from geometri.bujursangkar import hitung_luas_bujur_sangkar
+
+print (hitung_luas_segitiga(3,6))
+#print (hitung_luas_bujur_sangkar(4))
